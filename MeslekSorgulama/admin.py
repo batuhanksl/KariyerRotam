eea7d9917b0fbc3fc5474f3b0811803f4aade837
batuhanksl <batuhankl57@gmail.com>
2025-02-06 14:39:19 +0300

    Migrations Başlangıç

--- MeslekSorgulama/admin.py
+++ MeslekSorgulama/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Meslek
 
 # Register your models here.
+@admin.register(Meslek)
+class MeslekAdmin(admin.ModelAdmin):
+    list_display = ('meslek_adi', 'ortalama_maas', 'ankete_katilan_sayisi',)
+    search_fields = ('meslek_adi',)
+    pass
--- MeslekSorgulama/models.py
+++ MeslekSorgulama/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Sektor(models.Model):
+    sektor = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.meslek.meslek
+    
+class Bolum(models.Model):
+    bolum = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.meslek.meslek
+    
+class Yetenek(models.Model):
+    yetenek = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.meslek.meslek
+    
+
+class Meslek(models.Model):
+    meslek_adi = models.CharField(max_length=100)
+    en_dusuk_maas = models.IntegerField()
+    ortalama_maas = models.IntegerField()
+    en_yuksek_maas = models.IntegerField()
+    ankete_katilan_sayisi = models.IntegerField()
+    Sektor = models.ForeignKey(Sektor, on_delete=models.CASCADE)
+    sektor_yuzde = models.IntegerField()
+    Bolum = models.ForeignKey(Bolum, on_delete=models.CASCADE)
+    bolum_yuzde = models.IntegerField()
+    Yetenek = models.ForeignKey(Yetenek, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.meslek
